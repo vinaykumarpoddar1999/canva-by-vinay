6426fc4174114d0659355c13d541b6c8c869256e
vinaykumarpoddar1999 <mailtovinay1999@gmail.com>
2024-05-16 00:32:04 +0530

    Homepage Design

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'local') {
 // app.use('/api', require('./routes/designRoutes'))
 // app.use('/api', require('./routes/authRoutes'))
 
-if (process.env.NODE_ENV === 'local') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "./frontend/dist")))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"))
