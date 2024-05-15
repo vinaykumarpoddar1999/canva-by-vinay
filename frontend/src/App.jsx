7bc8f1d8582f3d4217a6a8dd68bc5ccc49241d11
vinaykumarpoddar1999 <mailtovinay1999@gmail.com>
2024-05-16 03:48:27 +0530

    Home Page Route Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //import Index from './pages/Index'
 import Layout from './pages/Layout'
+import Home from './components/Home'
+import Projects from './components/Projects'
+import Templates from './components/Templates'
 function App() {
   const router = createBrowserRouter([
     // {
@@ -11,6 +14,20 @@ function App() {
     {
       path: "/",
       element: <Layout />,
+      children:[
+        {
+          path:'/',
+          element:<Home />
+        },
+        {
+          path:'/templates',
+          element:<Templates />
+        },
+        {
+          path:'/projects',
+          element:<Projects />
+        }
+      ]
     },
   ]);
   return  <RouterProvider router={router} />
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import canvalogo from "../../public/canva-logo.svg";
 import profile from "../../public/profile.jpg";
-import { Link } from "react-router-dom";
+import { Link, Outlet, useLocation} from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { BsFolder, BsGrid1X2 } from "react-icons/bs";
 const Layout = () => {
+    const {pathname} = useLocation()
   const [show, setShow] = useState(false);
   return (
     <div className="bg-[#18191b] min-h-screen w-full">
@@ -59,6 +62,54 @@ const Layout = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="w-full flex mt-16">
+      <div className="sidebar w-[300px] p-5 h-[calc(100vh-70px)] fixed">
+        <div className="px-2 py-2 flex justify-start gap-5 items-center mb-3">
+          <img
+            src={profile}
+            className="w-[40px] h-[40px] rounded-full"
+            alt="profile image"
+          />
+          <div className="flex justify-center flex-col items-start">
+            <span className="text-[#e0dddd] font-bold text-md">
+              Vinay Poddar
+            </span>
+            <span className="text-[#c4c0c0] text-sm">Free</span>
+          </div>
+        </div>
+        <ul className="px-4 flex flex-col gap-2">
+          <li>
+            <Link to="/" className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${pathname==='/' ? 'bg-[#ffffff26]' : ''} rounded-[4px]`}>
+              <span className="text-xl">
+                <FaHome />
+              </span>
+              <span className="font-medium">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects"className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${pathname==='/projects' ? 'bg-[#ffffff26]' : ''} rounded-[4px]`}>
+              <span className="text-xl">
+                <BsFolder />
+              </span>
+              <span className="font-medium">Projects</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/templates" className={`text-[#e0dddd] px-2 py-2 flex justify-start items-center gap-2 ${pathname==='/templates' ? 'bg-[#ffffff26]' : ''} rounded-[4px]`}>
+              <span className="text-xl">
+                <BsGrid1X2 />
+              </span>
+              <span className="font-medium">Templates</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="ml-[300px] w-[calc(100%-300px)]">
+        <div className="py-4 pr-4">
+<Outlet />
+        </div>
+      </div>
       </div>
     </div>
   );
